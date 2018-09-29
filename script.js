3008353f1bf3c10513d461d75069145cf77c63d3
Andra <andra.popa1989@gmail.com>
2018-09-29 23:47:39 +0100

    removed background

--- script.js
+++ script.js
@@ -5,36 +5,15 @@
     document.addEventListener("mousemove", function(event) {
     	let canvas = document.getElementById("canvas");
     	let ctx = canvas.getContext("2d");
-        
-        
-        
-        
-        
-        
-        // background
-    	ctx.fillStyle = "#63a89e";
-    	ctx.fillRect(0, 0, canvas.width, canvas.height);
-
+    
+        // get mouse position
     	let mouseX = event.clientX;
     	let mouseY = event.clientY;
-    	ctx.fillStyle = "#333";
-    	let text = ctx.fillText("Hi", mouseX, mouseY);
-
+    	
+        // begin drawing
     	ctx.beginPath();
-    	ctx.fillStyle = "#d36025";
-		ctx.arc(mouseX,mouseY, 20, 0, 2*Math.PI);
+    	ctx.fillStyle = "#ddd";
+		ctx.arc(mouseX, mouseY, 15, 0, 2*Math.PI);
+		ctx.fill();
         
-        
-		ctx.stroke();
-        
-        ctx.fillStyle = "#d36";
-        ctx.beginPath();
-        ctx.arc(canvas.width / 2 - 300, canvas.height / 2 - 400, 70, 0, 2 * Math.PI);
-        ctx.arc(canvas.width / 2 - 250, canvas.height / 2 - 400, 70, 0, 2 * Math.PI);
-        ctx.fill();
-        
-        fill(48, 130, 31);
-        rect(176, 103, 12, 32);
-        
-    	container.textContent = text;
     });
