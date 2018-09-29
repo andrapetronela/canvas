21fb7b453a9f0f5c0e110ae88926162ba4f739aa
Andra <andra.popa1989@gmail.com>
2018-09-29 23:27:02 +0100

    fruit added

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
     let container = document.getElementById("container");
+
+    
     
     document.addEventListener("mousemove", function(event) {
     	let canvas = document.getElementById("canvas");
     	let ctx = canvas.getContext("2d");
-
+        
+        
+        
+        
+        
+        
+        // background
     	ctx.fillStyle = "#63a89e";
     	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     	let mouseX = event.clientX;
     	let mouseY = event.clientY;
     	ctx.fillStyle = "#333";
-    	let rect = ctx.fillText("Hi", mouseX, mouseY);
+    	let text = ctx.fillText("Hi", mouseX, mouseY);
 
     	ctx.beginPath();
     	ctx.fillStyle = "#d36025";
-		ctx.arc(mouseX,mouseY,20,0,2*Math.PI);
+		ctx.arc(mouseX,mouseY, 20, 0, 2*Math.PI);
+        
+        
 		ctx.stroke();
-    	container.textContent = rect;
-    })
-
-
+        
+        ctx.fillStyle = "#d36";
+        ctx.beginPath();
+        ctx.arc(canvas.width / 2 - 300, canvas.height / 2 - 400, 70, 0, 2 * Math.PI);
+        ctx.arc(canvas.width / 2 - 250, canvas.height / 2 - 400, 70, 0, 2 * Math.PI);
+        ctx.fill();
+        
+        fill(48, 130, 31);
+        rect(176, 103, 12, 32);
+        
+    	container.textContent = text;
+    });
