db543d28a52f421be3ffc43cc27539e8f47c7729
Andra <andra.popa1989@gmail.com>
2018-09-30 21:27:30 +0100

    click event added

--- script.js
+++ script.js
@@ -12,8 +12,23 @@
     	
         // begin drawing
     	ctx.beginPath();
-    	ctx.fillStyle = "#ddd";
-		ctx.arc(mouseX, mouseY, 15, 0, 2*Math.PI);
-		ctx.fill();
-        
+    	ctx.strokeStyle = "rgba(221, 221, 221, 0.83)";
+		ctx.arc(mouseX, mouseY, 8, 0, 2*Math.PI);
+		ctx.stroke();
     });
+
+    document.addEventListener("click", function(event) {
+        let canvas = document.getElementById("canvas");
+    	let ctx = canvas.getContext("2d");
+    
+        // get mouse position
+    	let mouseX = event.clientX;
+    	let mouseY = event.clientY;
+    	
+        // begin drawing
+    	ctx.beginPath();
+    	ctx.fillStyle = "#33dd95";
+		ctx.arc(mouseX, mouseY, 10, 0, 2*Math.PI);
+		ctx.fill();
+    });
+
